1b17f4567a62903ac47c1851d67bbf3db0d99b0c
801PC-06\student <23>
2026-02-06 12:15:35 +0900

    oroti

--- Assets/Script/Boss/Oroti/OrotiAttack_Shoot.cs
+++ Assets/Script/Boss/Oroti/OrotiAttack_Shoot.cs
@@ -1,23 +1,57 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Oroti/Attack/Shoot")]
 public class OrotiAttack_Shoot : OrotiAttackBase
 {
-    public override bool Execute(
-            List<OrotiNeck> allNecks,
-            Transform player,
-            OrotiController controller)
-    {
-        var selected = SelectNecks(allNecks);
-        if (selected.Count == 0)
-            return false;
+	[Header("Shoot Count")]
+	[SerializeField] private int bulletsPerNeck = 3;
 
-        foreach (var neck in selected)
-        {
-            neck.PlayShoot();
-        }
+	[Header("Bullet Interval")]
+	[SerializeField] private float bulletInterval = 0.15f;
 
-        return true;
-    }
+	[Header("Sequential Interval")]
+	[SerializeField] private float shootInterval = 0.4f;
+
+	public override bool Execute(
+		List<OrotiNeck> allNecks,
+		Transform player,
+		OrotiController controller)
+	{
+		var selected = SelectNecks(allNecks);
+		var attackable = FilterAttackable(selected);
+
+		if (attackable.Count == 0)
+			return false;
+
+		switch (attackOrder)
+		{
+			case NeckAttackOrderType.Simultaneous:
+				foreach (var neck in attackable)
+				{
+					neck.PlayShoot(bulletsPerNeck, bulletInterval);
+				}
+				break;
+
+			case NeckAttackOrderType.Sequential:
+				controller.StartSequentialAttack(
+					SequentialShootCoroutine(attackable)
+				);
+				break;
+		}
+
+		return true;
+	}
+
+	private IEnumerator SequentialShootCoroutine(List<OrotiNeck> necks)
+	{
+		foreach (var neck in necks)
+		{
+			if (neck.CanAttack)
+				neck.PlayShoot(bulletsPerNeck, bulletInterval);
+
+			yield return new WaitForSeconds(shootInterval);
+		}
+	}
 }
