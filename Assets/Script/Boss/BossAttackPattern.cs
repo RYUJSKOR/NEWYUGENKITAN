34cb7eaaa0ebfd04697f509b0bd0b5449582d723
RYUJSKOR <23cu0239@jec.ac.jp>
2026-02-13 03:12:49 +0900

    デバッグキー

--- Assets/Script/Boss/BossAttackPattern.cs
+++ Assets/Script/Boss/BossAttackPattern.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public abstract class BossAttackPattern : ScriptableObject
 {
-    [Header("アニメーション設定")]
+    [Header("アニメ?ション設定")]
     // ▼▼▼ 以下に [Tooltip("説明文")] を追加しました ▼▼▼
-    [Tooltip("攻撃の準備段階（狙う、振りかぶるなど）での手の形")]
+    [Tooltip("攻撃の?備段階（?う、振りかぶるなど）での手の?")]
     public ArmAnimationState prepareHandState = ArmAnimationState.OpenHand;
 
-    [Tooltip("攻撃がヒットする瞬間（叩きつけ、ビンタなど）での手の形")]
+    [Tooltip("攻撃がヒットする瞬間（?きつけ、ビン?など）での手の?")]
     public ArmAnimationState actionHandState = ArmAnimationState.Fist;
 
-    [Tooltip("攻撃後に待機位置へ戻る際の手の形")]
+    [Tooltip("攻撃後に待?位置へ戻る際の手の?")]
     public ArmAnimationState returnHandState = ArmAnimationState.Default;
     // ▲▲▲ ここまで ▲▲▲
 
---
+++ Assets/Script/Game/GlobalDebugManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class GlobalDebugManager : MonoBehaviour
+{
+    // 어디서든 접근 가능한 싱글톤 인스턴스
+    public static GlobalDebugManager Instance { get; private set; }
+    private GameData gameData;
+    BossTimerManager bossTimerManager;
+    private BulletSkill bulletskill;
+    private PlayerStateMachine playerstate;
+
+    public string titleSceneName = "TitleScene";
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        playerstate = FindAnyObjectByType<PlayerStateMachine>();
+        bossTimerManager = FindAnyObjectByType<BossTimerManager>();
+        gameData = FindAnyObjectByType<GameData>();
+        if (playerstate != null)
+        {
+            bulletskill = playerstate.GetStateByBaseClass<BulletSkill>();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ReturnToTitle();
+        }
+    }
+
+    public void ReturnToTitle()
+    {
+        if (GameData.Instance != null)
+            GameData.Instance.ResetAll();
+
+        if (bulletskill != null)
+        {
+            bulletskill.SetGauge(0f);
+        }
+
+        if (BossGameManager.Instance != null)
+        {
+            BossGameManager.Instance.ResetSavedData();
+        }
+
+        if (gameData != null && bossTimerManager != null)
+        {
+            gameData.saveBossTime(0.0f);
+            bossTimerManager.ResetTimer(0.0f);
+        }
+
+        if (bossTimerManager != null)
+        {
+            bossTimerManager.StopTimer(); // 最終時間を保存
+            Destroy(bossTimerManager.gameObject); // タイマーオブジェクトを削除
+        }
+
+
+        SceneManager.LoadScene("Title");
+    }
+}
